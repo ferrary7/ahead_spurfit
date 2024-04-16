93395be436a2df1d442481a438f6a5ce4b11ac3b
ferrary7 <ary7sharma@gmail.com>
2024-04-17 02:18:36 +0530

    navbar component created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 bg-white z-[100]">
       <header className="flex justify-between h-20 items-center py-3 px-20 w-screen">
-          <Image width={50} height={50} alt="" src="/ahead-logo.png" />
+          <Image width={50} height={50} alt="" src="/assets/ahead-logo.png" />
           <ul className="flex gap-6 items-center">
             <li className="cursor-pointer font-semibold ">Emotions</li>
             <li className="cursor-pointer font-semibold ">Manifesto</li>
